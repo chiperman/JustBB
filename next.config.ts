b7d00b886d6bb0856ac8f38799bb53951a7991b9
chiperman <chipermanju@gmail.com>
2026-04-07 15:09:11 +0800

    docs(guide): 更新功能列表并授权 Unsplash 图片资源

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,14 @@ if (process.env.NODE_ENV !== 'development' && process.env.SKIP_ENV_VALIDATION !=
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
   turbopack: {
     root: '.'
   }
